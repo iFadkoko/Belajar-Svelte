d91b20a38afabe29d55d6f37144de71798b23507
iFadkoko <fadlidx1@gmail.com>
2026-04-10 21:10:55 +0700

    Checkpoint no 1

---
+++ src/App.svelte
@@ -0,0 +1,15 @@
+<script>
+  import svelteLogo from './assets/svelte.svg'
+  import viteLogo from './assets/vite.svg'
+  import heroImg from './assets/hero.png'
+  import Counter from './lib/Counter.svelte'
+  import Hero from './lib/Gate.svelte'
+</script>
+
+<section id="center">
+  <Hero />
+  <Counter/>
+</section>
+
+<div class="ticks"></div>
+
---
+++ src/lib/Gate.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+    let isOpen = $state(false)
+    function toggle () {
+        isOpen = !isOpen
+    }
+</script>
+<section class="gate">
+    {#if !isOpen}  
+        <button class="gate-btn " onclick={toggle}>
+        Gate Dark souls
+        </button>
+    {/if}
+    {#if isOpen}
+        <div class="gate-content">
+            <p>Welcome to the world of Dark Souls!</p>
+            <p>Prepare to die...</p>
+        </div>
+    {/if}
+</section>
